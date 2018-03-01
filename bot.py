a81259381e396bad0b66f8ac47d93bc776aaff56
daryanovi <darya.novikova@gmail.com>
2018-03-01 13:53:32 +0300

    My first bot with Greeting

---
+++ bot.py
@@ -0,0 +1,33 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def start_bot(bot, update):
+	print(update)
+	bottext="""Привет, {}!
+Я бот Даши и понимаю простые команды. Для начала - команду {}""".format(update.message.chat.first_name, '/start')
+	update.message.reply_text(bottext)
+
+def chat(bot, update):
+	text=update.message.text
+	logging.info(text)
+	update.message.reply_text(text)
+
+def main():
+	updtr=Updater(settings.TELEGRAM_API_BOT_KEY)
+
+	updtr.dispatcher.add_handler(CommandHandler("Start", start_bot))
+	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
+
+	updtr.start_polling()
+	updtr.idle()
+
+if __name__=="__main__":
+	logging.info("Bot started")
+	main()
